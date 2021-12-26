d4068709d74b00adce9c1c76e6b45ffa831a7688
clarklemke <clark.lemke@gmail.com>
2021-12-26 16:42:52 +0000

    added envs to ignore

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,12 +1,13 @@
 from .base import *  # noqa
 from .base import env
 
+DEBUG = True
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["vanbel.ga"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["vanbelu.ga"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
--- landlord/utils/storages.py
+++ landlord/utils/storages.py
@@ -3,7 +3,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticRootS3Boto3Storage(S3Boto3Storage):
     location = "static"
-    default_acl = "public-read"
+    # default_acl = "public-read"
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
