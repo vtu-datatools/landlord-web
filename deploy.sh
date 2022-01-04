faa7d10baf1bbdaade5925ead6ed37deadf99890
clarklemke <clark.lemke@gmail.com>
2022-01-04 23:21:24 +0100

    put quotes around variables for export

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,15 @@
 # End script on any error
 set -e
 
-TEST="hello_this_is_a_test"
-
 # Pass AWS SSH key from github actions env variables to keyfile on runner
 echo "$PRIVATE_KEY" > private_key && chmod 600 private_key
 SSH="ssh -o StrictHostKeyChecking=no -i private_key ${USER_NAME}@${HOSTNAME}"
 
 # SSH onto EC2, pull latest main branch and deploy
 $SSH << EOF
-    export DJANGO_AWS_ACCESS_KEY_ID=${DJANGO_AWS_ACCESS_KEY_ID} &&
-    export DJANGO_AWS_SECRET_ACCESS_KEY=${DJANGO_AWS_SECRET_ACCESS_KEY} &&
-    export DJANGO_AWS_STORAGE_BUCKET_NAME=${DJANGO_AWS_STORAGE_BUCKET_NAME} &&
+    export DJANGO_AWS_ACCESS_KEY_ID="${DJANGO_AWS_ACCESS_KEY_ID}" &&
+    export DJANGO_AWS_SECRET_ACCESS_KEY="${DJANGO_AWS_SECRET_ACCESS_KEY}" &&
+    export DJANGO_AWS_STORAGE_BUCKET_NAME="${DJANGO_AWS_STORAGE_BUCKET_NAME}" &&
     cd app &&
     git checkout main &&
     git fetch --all &&
