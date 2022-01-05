43a661a183ef8e664db9f781140c909639ca4651
clarklemke <clark.lemke@gmail.com>
2022-01-05 10:04:26 +0100

    debug

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,9 @@ set -u
 # Pass AWS SSH key from github actions env variables to keyfile on runner
 echo "$PRIVATE_KEY" > private_key && chmod 600 private_key
 SSH="ssh -o StrictHostKeyChecking=no -i private_key ${USER_NAME}@${HOSTNAME}"
-echo $HOSTNAME
+echo "USER_NAME: $USER_NAME"
+echo "HOSTNAME: $HOSTNAME"
+echo "DJANGO_AWS_STORAGE_BUCKET_NAME: $DJANGO_AWS_STORAGE_BUCKET_NAME"
 # SSH onto EC2, pull latest main branch and deploy
 if [ -n "${DJANGO_AWS_STORAGE_BUCKET_NAME-a}" ]; then
   echo >&2 "Fatal error: DJANGO_AWS_STORAGE_BUCKET_NAME not set"
