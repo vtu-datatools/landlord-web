65d134d03afef3f61c0d6096aca2f39ebafd5edd
clarklemke <clark.lemke@gmail.com>
2022-01-06 11:01:23 +0100

    refactor to use ssh-action

--- deploy.sh
+++ deploy.sh
@@ -5,20 +5,9 @@ set -e
 # End script on unset variable error
 set -u
 
-# Pass AWS SSH key from github actions env variables to keyfile on runner
-echo "$PRIVATE_KEY" > private_key && chmod 600 private_key
-
-SSH="ssh -o StrictHostKeyChecking=no -i private_key ${USER_NAME}@${HOSTNAME}"
-
-
-$SSH << EOF
-    cd app 
-    git checkout main 
-    git fetch --all 
-    git reset --hard origin/main 
-    git pull origin main 
-    echo "DJANGO_AWS_ACCESS_KEY_ID=$DJANGO_AWS_ACCESS_KEY_ID" >> .envs/.production/.django
-    echo "DJANGO_AWS_SECRET_ACCESS=$DJANGO_AWS_SECRET_ACCESS" >> .envs/.production/.django
-    echo "DJANGO_AWS_STORAGE_BUCKET_NAME=$DJANGO_AWS_STORAGE_BUCKET_NAME" >> gs.envs/.production/.django
-    docker-compose -f docker-production.yaml up -d --build
-EOF
+cd app 
+git checkout main 
+git fetch --all 
+git reset --hard origin/main 
+git pull origin main
+docker-compose -f docker-production.yaml up -d --build
