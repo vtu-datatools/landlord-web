ab8cc56ff8682d967c38a459a54d163471f5f23e
clarklemke <clark.lemke@gmail.com>
2022-02-02 13:05:55 -0800

    moved s3 location to root

--- landlord/utils/storages.py
+++ landlord/utils/storages.py
@@ -2,7 +2,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticRootS3Boto3Storage(S3Boto3Storage):
-    location = ""
+    location = "static"
     # default_acl = "public-read"
 
 
