2b32cf9bf7a1ca1fb00ed687e82c8746b028bac3
clarklemke <clark.lemke@gmail.com>
2022-03-04 14:12:24 -0800

    fixed api root

--- apps/landlord/api.py
+++ apps/landlord/api.py
@@ -5,6 +5,6 @@ from rest_framework import routers
 from .viewsets import CurrentIssuesViewSet
 
 router = routers.DefaultRouter()
-router.register(r"landlord", CurrentIssuesViewSet)
+router.register(r"issues", CurrentIssuesViewSet)
 
 urlpatterns = router.urls
