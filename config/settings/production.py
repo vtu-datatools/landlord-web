f920feeb85d526f8b5aa32e2922b8756cc7b5a26
clarklemke <clark.lemke@gmail.com>
2022-03-22 12:05:50 -0700

    changed default email subject line

--- config/settings/production.py
+++ config/settings/production.py
@@ -97,7 +97,7 @@ SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = env(
     "DJANGO_EMAIL_SUBJECT_PREFIX",
-    default="[landlord]",
+    default="[VTU Vancouver Landlords]",
 )
 
 # ADMIN
