3f27d530ac8c047fdff258465ee22ca03961e9e5
clarklemke <clark.lemke@gmail.com>
2022-06-20 20:08:28 -0700

    fixed mobile profile avatar shrink

--- frontend/src/components/profile/index.js
+++ frontend/src/components/profile/index.js
@@ -1,43 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import Avatar from "../avatar";
 import "./styles.css";
 
-class Profile extends Component {
-  formatDateTime(datetime) {
-    return datetime.split(".")[0].replace("T", " ");
-  }
+const Profile = (props) => {
+  const { name, username, avatar, bio, status, isStaff, dateJoined } = props;
 
-  render() {
-    const { name, username, avatar, bio, status, isStaff, dateJoined } =
-      this.props;
-
-    return (
-      <div className="profileContainer">
-        <div>
-          <Avatar className="profileAvatar" avatar={avatar} centered={false} />
+  return (
+    <div className="profileContainer">
+      <div className="avatarContainer">
+        <Avatar className="profileAvatar" avatar={avatar} centered={false} />
+      </div>
+      <div className="profileInfo">
+        <div className="name">{name}</div>
+        <div className="username">
+          <strong>@{username}</strong>
+          <b className="staffStatus">{isStaff ? " (Staff) " : ""}</b>
         </div>
-        <div className="profileInfo">
-          <div className="name">{name}</div>
-          <div className="username">
-            <strong>@{username}</strong>
-            <b className="staffStatus">{isStaff ? " (Staff) " : ""}</b>
-          </div>
-          <div className="status">
-            <strong>Status: </strong>
-            {status}
-          </div>
-          <div className="dateJoined">
-            <strong>Joined: </strong>
-            {dateJoined}
-          </div>
-          <div className="bio">
-            <strong>Bio: </strong>
-            {bio}
-          </div>
+        <div className="status">
+          <strong>Status: </strong>
+          {status}
+        </div>
+        <div className="dateJoined">
+          <strong>Joined: </strong>
+          {dateJoined}
+        </div>
+        <div className="bio">
+          <strong>Bio: </strong>
+          {bio}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Profile;
